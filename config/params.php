42c8bf6c07a7d4f007a0c36e6aa49c262d495fa8
Chris <chris@clyates.plus.com>
2025-04-02 15:48:54 +0100

    Use constants

--- config/params.php
+++ config/params.php
@@ -2,30 +2,63 @@
 
 declare(strict_types=1);
 
-use BeastBytes\Yii\Totp\Totp;
+use BeastBytes\Yii\Totp\CryptCipher;
+use BeastBytes\Yii\Totp\CryptKdfAlgorithm;
+use BeastBytes\Yii\Totp\TotpDigest;
+
+/**
+ * // Number of backup codes to generate
+ */
+const BACKUP_CODE_COUNT = 10;
+/**
+ * Length of each backup code
+ */
+const BACKUP_CODE_LENGTH = 16;
+/**
+ * Derivation iterations count
+ */
+const CRYPT_ITERATIONS = 100000;
+/**
+ * Encryption/Decryption key. Should be changed
+ */
+const CRYPT_KEY = 'rstswSLBfzMKLyELbvKSkfT3qkHjAVoSQtVNSkWimeiGbOABqJXA3NqFYtXyewpS';
+
+/**
+ * Number of digits in the OTP code
+ */
+const TOTP_DIGITS = 6;
+/**
+ * Number of seconds that an OTP code is valid before or after the period
+ * to allow for clock drift between client and server
+ */
+const TOTP_LEEWAY = 2;
+/**
+ * Number of seconds in the OTP period, i.e. how often the OTP code is generated
+ */
+const TOTP_PERIOD = 30;
 
 return [
     'beastbytes/yii-totp' => [
         'backupCodes' => [
-            'count' => 10, // number of backup codes to generate
-            'length' => 16, // length of each backup code
+            'count' => BACKUP_CODE_COUNT,
+            'length' => BACKUP_CODE_LENGTH,
         ],
         'crypt' => [
             'authorizationKeyInfo' => 'TotpAuthorizationKey',
-            'cipher' => 'AES-128-CBC', // Encryption cipher: AES-128-CBC, AES-192-CBC, AES-256-CBC
-            'iterations' => 100000,
-            'kdfAlgorithm' => 'sha256', // sha256, sha384, sha512
-            'secret' => 'rstswSLBfzMKLyELbvKSkfT3qkHjAVoSQtVNSkWimeiGbOABqJXA3NqFYtXyewpS', // should be changed
+            'cipher' => CryptCipher::AES_128_CBC, // Encryption cipher: AES-128-CBC, AES-192-CBC, AES-256-CBC
+            'iterations' => CRYPT_ITERATIONS,
+            'kdfAlgorithm' => CryptKdfAlgorithm::sha256->name, // sha256, sha384, sha512
+            'key' => CRYPT_KEY,
         ],
         'database' => [
             'otpBackupCodesTable' => 'otp_backup_codes',
             'totpTable' => 'totp',
         ],
         'totp' => [ // The default values work for authenticator apps like Google Authenticator
-            'digest' => Totp::DIGEST, // HMAC digest algorithm; sha1, sha256, sha512
-            'digits' => Totp::DIGITS, // number of digits in the OTP code
-            'leeway' => Totp::LEEWAY, // leeway in seconds that an OTP code is valid before or after the period; allows time drift; must be less than $period
-            'period' => Totp::PERIOD, // OTP time period - how often a new code is generated - in seconds
+            'digest' => TotpDigest::sha1->name, // HMAC digest algorithm for TOTP
+            'digits' => TOTP_DIGITS,
+            'leeway' => TOTP_LEEWAY,
+            'period' => TOTP_PERIOD,
         ]
     ],
 ];
