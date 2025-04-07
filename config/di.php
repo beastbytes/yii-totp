61f4bb68811c0b0b9798ce97ab1abda941591817
Chris <chris@clyates.plus.com>
2025-04-07 17:07:38 +0100

    Update configuration

--- config/di.php
+++ config/di.php
@@ -2,35 +2,55 @@
 
 declare(strict_types=1);
 
+use BeastBytes\Yii\Totp\Totp;
 use BeastBytes\Yii\Totp\TotpService;
 use Psr\Clock\ClockInterface;
 use Symfony\Component\Clock\Clock;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Security\PasswordHasher;
+use Yiisoft\Definitions\Reference;
+use Yiisoft\Security\Crypt;
 
 /** @var array $params */
 
 return [
     ClockInterface::class => Clock::class,
+    Crypt::class => [
+        'class' => Crypt::class,
+        '__construct()' => [
+            'cipher' => $params['beastbytes/yii-totp']['crypt']['cipher'],
+        ],        
+        'withDerivationIterations()' => [
+            $params['beastbytes/yii-totp']['crypt']['iterations'],
+        ],
+        'withKdfAlgorithm()' => [
+            $params['beastbytes/yii-totp']['crypt']['kdfAlgorithm'],
+        ],    
+        'withAuthorizationKeyInfo' => [
+            $params['beastbytes/yii-totp']['crypt']['authorizationKeyInfo'],
+        ],    
+    ],
+    Totp::class => [
+        'class' => Totp::class,
+        '__construct()' => [
+            'clock' => ClockInterface::class,
+            'digest' => $params['beastbytes/yii-totp']['totp']['digest'],
+            'digits' => $params['beastbytes/yii-totp']['totp']['digits'],
+            'leeway' => $params['beastbytes/yii-totp']['totp']['leeway'],
+            'period' => $params['beastbytes/yii-totp']['totp']['period'],
+            'secretLength' => $params['beastbytes/yii-totp']['totp']['secretLength'],
+        ],
+    ],
     TotpService::class => [
         'class' => TotpService::class,
         '__construct()' => [
-            'clock' => ClockInterface::class,
+            'backupCodeCount' => $params['beastbytes/yii-totp']['backupCode']['count'],
+            'backupCodeLength' => $params['beastbytes/yii-totp']['backupCode']['length'],
+            'crypt' => Reference::to(Crypt::class),
             'database' => ConnectionInterface::class,
-            'otpBackupCodesTable' => $params['beastbytes/yii-totp']['database']['otpBackupCodesTable'],
+            'encryptionKey' => $params['beastbytes/yii-totp']['crypt']['key'],
+            'otpBackupCodesTable' => $params['beastbytes/yii-totp']['database']['otpBackupCodeTable'],
+            'totp' => Reference::to(Totp::class),
             'totpTable' => $params['beastbytes/yii-totp']['database']['totpTable'],
-            'totpDigest' => $params['beastbytes/yii-totp']['totp']['digest'],
-            'totpDigits' => $params['beastbytes/yii-totp']['totp']['digits'],
-            'totpLeeway' => $params['beastbytes/yii-totp']['totp']['leeway'],
-            'totpPeriod' => $params['beastbytes/yii-totp']['totp']['period'],
-            'backupCodeCount' => $params['beastbytes/yii-totp']['backupCodes']['count'],
-            'backupCodeLength' => $params['beastbytes/yii-totp']['backupCodes']['length'],
-            'passwordHasher' => PasswordHasher::class,
-            'cipher' => $params['beastbytes/yii-totp']['crypt']['cipher'],
-            'iterations' => $params['beastbytes/yii-totp']['crypt']['iterations'],
-            'kdfAlgorithm' => $params['beastbytes/yii-totp']['crypt']['kdfAlgorithm'],
-            'authorizationKeyInfo' => $params['beastbytes/yii-totp']['crypt']['authorizationKeyInfo'],
-            'key' => $params['beastbytes/yii-totp']['crypt']['key'],
         ],
     ]
 ];
--- config/params.php
+++ config/params.php
@@ -4,64 +4,35 @@ declare(strict_types=1);
 
 use BeastBytes\Yii\Totp\CryptCipher;
 use BeastBytes\Yii\Totp\CryptKdfAlgorithm;
+use BeastBytes\Yii\Totp\Totp;
 use BeastBytes\Yii\Totp\TotpDigest;
+use BeastBytes\Yii\Totp\TotpService;
 use OTPHP\OTPInterface;
-use OTPHP\TOTP;
 use OTPHP\TOTPInterface;
-
-/**
- * // Number of backup codes to generate
- */
-const BACKUP_CODE_COUNT = 10;
-/**
- * Length of each backup code
- */
-const BACKUP_CODE_LENGTH = 16;
-/**
- * Derivation iterations count
- */
-const CRYPT_ITERATIONS = 100000;
-/**
- * Encryption/Decryption key. Should be changed
- */
-const CRYPT_KEY = 'rstswSLBfzMKLyELbvKSkfT3qkHjAVoSQtVNSkWimeiGbOABqJXA3NqFYtXyewpS';
-
-/**
- * Number of digits in the OTP code
- */
-const TOTP_DIGITS = 6;
-/**
- * Number of seconds that an OTP code is valid before or after the period
- * to allow for clock drift between client and server
- */
-const TOTP_LEEWAY = 2;
-/**
- * Number of seconds in the OTP period, i.e. how often the OTP code is generated
- */
-const TOTP_PERIOD = 30;
 
 return [
     'beastbytes/yii-totp' => [
-        'backupCodes' => [
-            'count' => BACKUP_CODE_COUNT,
-            'length' => BACKUP_CODE_LENGTH,
+        'backupCode' => [
+            'count' => TotpService::BACKUP_CODE_COUNT,
+            'length' => TotpService::BACKUP_CODE_LENGTH,
         ],
         'crypt' => [
             'authorizationKeyInfo' => 'TotpAuthorizationKey',
             'cipher' => CryptCipher::AES_128_CBC, // Encryption cipher: AES-128-CBC, AES-192-CBC, AES-256-CBC
-            'iterations' => CRYPT_ITERATIONS,
+            'iterations' => TotpService::CRYPT_ITERATIONS,
             'kdfAlgorithm' => CryptKdfAlgorithm::sha256->name, // sha256, sha384, sha512
-            'key' => CRYPT_KEY,
+            'key' => TotpService::CRYPT_KEY,
         ],
         'database' => [
-            'otpBackupCodesTable' => 'otp_backup_codes',
+            'otpBackupCodeTable' => 'otp_backup_code',
             'totpTable' => 'totp',
         ],
         'totp' => [ // The default values work for authenticator apps like Google Authenticator
-            'digest' => TotpDigest::sha1->name, // HMAC digest algorithm for TOTP
+            'digest' => TotpDigest::sha1->name,
             'digits' => OTPInterface::DEFAULT_DIGITS,
-            'leeway' => TOTP_LEEWAY,
+            'leeway' => TOTP::DEFAULT_LEEWAY,
             'period' => TOTPInterface::DEFAULT_PERIOD,
+            'secretLength' => TOTP::DEFAULT_SECRET_LENGTH,
         ]
     ],
 ];
