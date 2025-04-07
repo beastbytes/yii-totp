68a27bbb786fd4a90ae84beea2d54908c51172f3
Chris <chris@clyates.plus.com>
2025-04-07 17:08:06 +0100

    Add db migrations

---
+++ migrations/uuid_pk/M250404162335CreateTables.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeastBytes\Yii\Totp\Migration\uuid_pk;
+
+use Yiisoft\Db\Exception\InvalidConfigException;
+use Yiisoft\Db\Exception\NotSupportedException;
+use Yiisoft\Db\Migration\MigrationBuilder;
+use Yiisoft\Db\Migration\RevertibleMigrationInterface;
+use Yiisoft\Db\Migration\TransactionalMigrationInterface;
+
+final class M250404162335CreateTables implements RevertibleMigrationInterface, TransactionalMigrationInterface
+{
+    private const BACKUP_CODE_TABLENAME = 'otp_backup_code';
+    private const TOTP_TABLENAME = 'totp';
+
+
+    /**
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     */
+    public function up(MigrationBuilder $b): void
+    {
+        $b->createTable(
+            self::BACKUP_CODE_TABLENAME,
+            [
+                'id' => 'string(255) NOT NULL',
+                'user_id' => 'string(255) NOT NULL',
+                'code' => 'string(255) NOT NULL',
+                'PRIMARY KEY ([[id]])',
+            ],
+        );
+        $b->createIndex(self::BACKUP_CODE_TABLENAME, 'idx-otp_backup_codes-user_id', 'user_id');
+
+        $b->createTable(
+            self::TOTP_TABLENAME,
+            [
+                'user_id' => 'string(255) NOT NULL',
+                'secret' => 'binary NOT NULL',
+                'digest' => 'string(255) NOT NULL',
+                'digits' => 'integer NOT NULL',
+                'leeway' => 'integer NOT NULL',
+                'period' => 'integer NOT NULL',
+                'last_code' => 'string(6) NOT NULL',
+                'PRIMARY KEY ([[user_id]])',
+            ],
+        );
+    }
+
+    /**
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     */
+    public function down(MigrationBuilder $b): void
+    {
+        $b->dropTable(self::BACKUP_CODE_TABLENAME);
+        $b->dropTable(self::TOTP_TABLENAME);
+    }
+}
